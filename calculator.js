ce755e74a8efd08ae09d83a823d1453149375f36
Musa Sartev <musaatidzhe@gmail.com>
2021-11-09 10:47:40 +0200

    js code completed

--- calculator.js
+++ calculator.js
@@ -1,22 +1,89 @@
-let myStr = 0;
+let myArr = [];
 function clickOnEq() {
-   document.getElementById('calculate-area').value = myStr;
+  if (myArr.includes("/")) {
+     const newArr = myArr.filter(el => {if(!isNaN(el)) return el});
+    document.getElementById("calculate-area").value = `${myArr.join(' ')} = ${newArr.reduce((total, el) => total /= el)}`;
+  }
 }
 
-myStr = 0;
+function clickOnDel() {}
 
 function clickOn7() {
-   myStr += 7;
-   document.getElementById('calculate-area').value = 7;
+  myArr.push(7);
+  document.getElementById("calculate-area").value = 7;
 }
 
-document.getElementById('calculate-area').value = myStr;
-
 function clickOn8() {
-   myStr += 8;
+  myArr.push(8);
+  document.getElementById("calculate-area").value = 8;
+}
+
+function clickOn9() {
+  myArr.push(9);
+  document.getElementById("calculate-area").value = 9;
+}
+
+function clickOn4() {
+  myArr.push(4);
+  document.getElementById("calculate-area").value = 4;
+}
+
+function clickOn5() {
+  myArr.push(5);
+  document.getElementById("calculate-area").value = 5;
+}
+
+function clickOn6() {
+  myArr.push(6);
+  document.getElementById("calculate-area").value = 6;
+}
+
+function clickOn1() {
+  myArr.push(1);
+  document.getElementById("calculate-area").value = 1;
+}
+
+function clickOn2() {
+  myArr.push(2);
+  document.getElementById("calculate-area").value = 2;
+}
+
+function clickOn3() {
+  myArr.push(3);
+  document.getElementById("calculate-area").value = 3;
+}
+
+function clickOn0() {
+  myArr.push(0);
+  document.getElementById("calculate-area").value = 0;
 }
 
 function clickOnReset() {
-   document.getElementById('calculate-area').value = 0;
-   myStr = 0;
+  document.getElementById("calculate-area").value = 0;
+  myArr = [];
+}
+
+function clickOnMin() {
+  document.getElementById("calculate-area").value = "-";
+  myArr.push("-");
+}
+
+function clickOnPlus() {
+  document.getElementById("calculate-area").value = "+";
+  myArr.push("+");
+}
+
+function clickOnPro() {
+  document.getElementById("calculate-area").value = "*";
+  myArr.push("*");
+}
+
+function clickOnDiv() {
+  document.getElementById("calculate-area").value = "/";
+  myArr.push("/");
+}
+
+function clickOnDec() {
+  document.getElementById("calculate-area").value = ".";
+  myArr.push(".");
 }
